10a53f421418fa772f6acca74eef6f222df44911
gHosttt <aylton.sitefane1@gmail.com>
2023-04-12 11:29:50 +0200

    Criacao do componente botao(adicionar, remover tarefa), do ficheiro de tarefas com tarefas estaticas

--- appmain/src/App.vue
+++ appmain/src/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header title="Tarefas" />
+    <Tasks @delete-task = "deleteTask" :tasks="tasks" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from "./components/Header.vue"
+import Tasks from "./components/Tasks"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+  },
+  data(){
+    return {
+      tasks: []
+    }
+  },
+  methods:{
+    deleteTask(id){
+      if (confirm('Tem a certeza?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+      
+
+    },
+  },
+  created(){
+    this.tasks = [
+      {
+        id:1,
+        text: 'Estudar para o teste',
+        dia: '25 abril 2023',
+        reminder: true,
+      },
+      {
+        id:2,
+        text: 'Reuniao familiar',
+        dia:'23 abril 2023',
+        reminder: true,
+      }
+      ,{
+        id:3,
+        text: 'Consulta no hospital',
+        dia:'24 de abril de 2023',
+        reminder: true,
+      }
+    ]
   }
 }
 </script>
@@ -23,4 +64,45 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+ * {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+ }
+
+ body{
+  font-family: 'Poppins', sans-serif;
+ }
+ .container{
+  max-width: 500px;
+  margin: 30px auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+
+ }
+
+ .btn{
+  display: inline-block;
+  background: #cdc7c7;
+  color: #000;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+ }
+
+ .btn:focus{
+  outline: none;
+ }
+
+ .btn:hover{
+   background-color: aqua;
+ }
 </style>
---
+++ appmain/src/components/Task.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>{{ task.text }}
+            <i @click="onDelete(task.id)" class="uil uil-trash"></i>
+        </h3>
+        <h3>{{ task.dia }}</h3>
+
+    </div>
+    
+</template>
+
+<script>
+    export default{
+        name:'Task',
+        props:{
+            task: Object,
+        },
+         methods:{
+            onDelete(id){
+                this.$emit('delete-task', id)
+            },
+        },
+    }
+</script>
+
+<style scope>
+    .task{
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+
+    .uil{
+        color: red;
+    }
+    .task.reminder{
+        border-left: 5px solid green;
+
+    }
+
+    .task h3{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
